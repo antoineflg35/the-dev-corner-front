212fbbe8ef7f566e0bf4fa44caf5f2d19c5a3b87
Antoine Flegeau <antoineflegeau35@gmail.com>
2022-09-21 13:49:36 +0200

    Installation Axios

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,16 @@
 // == Import
+import { useEffect } from 'react';
 import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 function App() {
+  useEffect(
+    () => {
+      console.log('exécuté après le rendu initial');
+    },
+    [],
+  );
   return (
     <div className="app">
       <img src={reactLogo} alt="react logo" />
