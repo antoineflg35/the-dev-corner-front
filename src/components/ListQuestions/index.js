7aa99fb37cd16bd9de037403fe53119d4f80bbdb
Antoine Flegeau <antoineflegeau35@gmail.com>
2022-09-26 11:37:22 +0200

    add questions closed & statistics of the week

--- src/components/ListQuestions/index.js
+++ src/components/ListQuestions/index.js
@@ -1,12 +1,14 @@
 import Questions from './Questions';
+import Informations from './Informations';
 import ButtonAskQuestion from '../Home/ButtonAskQuestion';
 import './styles.scss';
 
 function ListQuestions() {
   return (
-    <div className="list_questions">
+    <div className="page_questions">
       <Questions />
       <ButtonAskQuestion />
+      <Informations />
     </div>
   );
 }
