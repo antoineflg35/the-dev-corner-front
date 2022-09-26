a31cdc456eb33fa501c452535b4b1a6bd3645f61
Antoine Flegeau <antoineflegeau35@gmail.com>
2022-09-26 13:45:18 +0200

    Update SCSS Add question

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,9 +7,9 @@ import { fetchQuestions } from '../../actions/questions';
 import Header from '../Header';
 import Home from '../Home';
 import ListQuestions from '../ListQuestions';
-import Footer from '../Footer';
+import AddQuestion from '../AddQuestion';
 import QuestionDetails from '../QuestionDetails';
-
+import Footer from '../Footer';
 // == Composant
 function App() {
   // const dispatch = useDispatch();
@@ -32,6 +32,10 @@ function App() {
         <Route
           path="/questions/details"
           element={<QuestionDetails />}
+        />
+        <Route
+          path="/questions/add"
+          element={<AddQuestion />}
         />
       </Routes>
       <Footer />
