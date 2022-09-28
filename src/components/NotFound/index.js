de961c0d70cf2d4691ce276a92407a8496836211
Ajarhou <jarhou06@gmail.com>
2022-09-28 21:01:37 +0200

    page error 404

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,21 @@
 // == Import
+import { Image } from 'semantic-ui-react';
+
+
+import logo from 'src/assets/404.jpg';
+
+
 import './styles.scss';
 
 // == Composant
 function NotFound() {
-  return <div className="not-found">404</div>;
+  return <div className="not-found">
+     <Image src={logo}  fluid />
+  </div>
 }
 
 // == Export
 export default NotFound;
+
+
+ 
